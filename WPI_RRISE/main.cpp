efc38a49111095bfe97515bb344dc96cfc36b02e
BBBBlarry <blarrywang@gmail.com>
2015-11-01 17:09:46 -0500

    Initial Commit

---
+++ WPI_RRISE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WPI_RRISE
+//
+//  Created by Blarry_Air on 2015-11-01.
+//  Copyright (c) 2015 RRISE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
